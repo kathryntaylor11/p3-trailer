07458df2055b42352021f84379d78db50b1bf302
Kat Taylor <KathrynTaylor1@BMO.PK5001Z>
2015-05-18 14:28:08 -0700

    Base Commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Media Element Player
+
+$('audio,video').mediaelementplayer({
+	success: function(player, node) {
+	  $('#' + node.id + '-mode').html('mode: ' + player.pluginType);
+	},
+	startLanguage: 'en',
+	translationSelector: true
+});
+
